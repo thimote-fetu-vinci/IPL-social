85f88ed409459a8ff789e8045778d1308689196b
Fétu Thimoté <thimote.fetu@student.vinci.be>
2025-01-17 16:49:56 +0100

    refactor: special characteres test

--- functions/passwordChecker.js
+++ functions/passwordChecker.js
@@ -1,5 +1,11 @@
 function isValidPassword(password) {
+    const specialCharsRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
     if (password.length < 8) {
+        return false;
+    }
+
+    if (!specialCharsRegex.test(password)) {
         return false;
     }
 
